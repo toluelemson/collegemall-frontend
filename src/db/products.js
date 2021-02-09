94dcf5a384a710ce2972b68c5e10fe8a054ca0d1
elemson <toluwani@juststart.dev>
2021-02-09 08:27:50 +0200

    db-sample

---
+++ src/db/products.js
@@ -0,0 +1,88 @@
+const ProductDB = [{
+
+	_id: 1,
+	name: 'Airpod',
+	images: './images/airpods.jpg',
+	description:
+	 'Bluetooth technology lets you connect it with compatible devices wirelesslly High-quality AAC audio offers immersive listening experience Bulit-in microphone allows',
+	brand: 'Apple',
+	category: 'electronics',
+	price: 89.9,
+	countInStock: 3,
+	rating: 4.5,
+	numReviews: 4
+
+},
+{
+	_id: '2',
+	name: 'Cannon E0S 80D DSLR Camera',
+	image: '/images/camera.jpg',
+	description:
+		'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+	brand: 'Cannon',
+	category: 'Electronics',
+	price: 929.99,
+	countInStock: 5,
+	rating: 4.5,
+	numReviews: 4
+
+},
+{
+	_id: '3',
+	name: 'Cannon E0S 80D DSLR Camera',
+	image: '/images/camera.jpg',
+	description:
+		'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+	brand: 'Cannon',
+	category: 'Electronics',
+	price: 929.99,
+	countInStock: 5,
+	rating: 2,
+	numReviews: 4
+
+},
+{
+	_id: '4',
+	name: 'Cannon E0S 80D DSLR Camera',
+	image: '/images/camera.jpg',
+	description:
+		'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+	brand: 'Cannon',
+	category: 'Electronics',
+	price: 929.99,
+	countInStock: 5,
+	rating: 4.5,
+	numReviews: 4
+
+},
+{
+	_id: '5',
+	name: 'Cannon E0S 80D DSLR Camera',
+	image: '/images/camera.jpg',
+	description:
+		'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+	brand: 'Cannon',
+	category: 'Electronics',
+	price: 929.99,
+	countInStock: 5,
+	rating: 4.5,
+	numReviews: 4
+
+},
+{
+	_id: '6',
+	name: 'Cannon E0S 80D DSLR Camera',
+	image: '/images/camera.jpg',
+	description:
+		'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+	brand: 'Cannon',
+	category: 'Electronics',
+	price: 929.99,
+	countInStock: 5,
+	rating: 4.5,
+	numReviews: 4
+
+}
+]
+
+export default ProductDB
